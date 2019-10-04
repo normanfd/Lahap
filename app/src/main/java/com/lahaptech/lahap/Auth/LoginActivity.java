1e8aecaace21dff8d5ddc6aad40ed3f2686cfdcb
normanfd <norman_fd@apps.ipb.ac.id>
2019-10-04 14:58:03 +0700

    second commit

---
+++ app/src/main/java/com/lahaptech/lahap/Auth/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.lahaptech.lahap.Auth;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.lahaptech.lahap.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
--- app/src/main/java/com/lahaptech/lahap/MainActivity.java
+++ app/src/main/java/com/lahaptech/lahap/MainActivity.java
@@ -2,15 +2,43 @@ package com.lahaptech.lahap;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import com.lahaptech.lahap.Auth.LoginActivity;
+import com.lahaptech.lahap.Auth.RegisterActivity;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @BindView(R.id.main_join_now_btn)
+    Button btn_join;
+    @BindView(R.id.main_login_btn)
+    Button btn_login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
 
+        btn_join.setOnClickListener(this);
+        btn_login.setOnClickListener(this);
+    }
 
+    @Override
+    public void onClick(View view) {
+        if (view.getId() == R.id.main_login_btn) {
+            Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+            startActivity(intent);
+        }
+        else{
+            Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
+            startActivity(intent);
+        }
     }
 }
