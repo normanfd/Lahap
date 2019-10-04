5a7847524b6d3516a421d809b854280092538ec9
gilangipb <gilang_gunawan@apps.ipb.ac.id>
2019-10-04 17:29:13 +0700

    add home user activity

---
+++ app/src/main/java/com/lahaptech/lahap/HomeUserActivity.java
@@ -0,0 +1,14 @@
+package com.lahaptech.lahap;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HomeUserActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_user);
+    }
+}
