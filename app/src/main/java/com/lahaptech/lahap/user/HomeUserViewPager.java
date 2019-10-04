0553605d93dad51b36e7480010eae4dc7f8a6dae
normanfd <norman_fd@apps.ipb.ac.id>
2019-10-04 19:59:16 +0700

    homeUser

---
+++ app/src/main/java/com/lahaptech/lahap/user/HomeUserViewPager.java
@@ -0,0 +1,38 @@
+package com.lahaptech.lahap.user;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+import java.util.ArrayList;
+
+public class HomeUserViewPager extends FragmentPagerAdapter {
+    private ArrayList<Fragment> fragments = new ArrayList<>();
+    private ArrayList<String> fragmentsTitle = new ArrayList<>();
+
+    public HomeUserViewPager(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int i) {
+        return fragments.get(i);
+    }
+
+    @Override
+    public int getCount() {
+        return fragments.size();
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return fragmentsTitle.get(position);
+    }
+
+    public void addFragment(Fragment fragment, String title) {
+        fragments.add(fragment);
+        fragmentsTitle.add(title);
+    }
+}
