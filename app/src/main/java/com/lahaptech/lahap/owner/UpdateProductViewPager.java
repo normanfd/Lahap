127df1b17b81a9a7db951aa1464768eaf7f5e0c3
gilangipb <gilang_gunawan@apps.ipb.ac.id>
2019-10-06 10:28:51 +0700

    add product update

---
+++ app/src/main/java/com/lahaptech/lahap/owner/UpdateProductViewPager.java
@@ -0,0 +1,38 @@
+package com.lahaptech.lahap.owner;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+import java.util.ArrayList;
+
+public class UpdateProductViewPager extends FragmentPagerAdapter {
+    private ArrayList<Fragment> fragments = new ArrayList<>();
+    private ArrayList<String> fragmentsTitle = new ArrayList<>();
+
+    public UpdateProductViewPager(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int i) {
+        return fragments.get(i);
+    }
+
+    @Override
+    public int getCount() {
+        return fragments.size();
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return fragmentsTitle.get(position);
+    }
+
+    public void addFragment(Fragment fragment, String title) {
+        fragments.add(fragment);
+        fragmentsTitle.add(title);
+    }
+}
