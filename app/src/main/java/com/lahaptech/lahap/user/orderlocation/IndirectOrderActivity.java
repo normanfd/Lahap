e563cfb227310047ec3070a7383da74850d83bfa
gilangipb <gilang_gunawan@apps.ipb.ac.id>
2019-10-11 21:22:43 +0700

    add time picker

--- app/src/main/java/com/lahaptech/lahap/user/orderlocation/IndirectOrderActivity.java
+++ app/src/main/java/com/lahaptech/lahap/user/orderlocation/IndirectOrderActivity.java
@@ -2,21 +2,57 @@ package com.lahaptech.lahap.user.orderlocation;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.app.TimePickerDialog;
+import android.os.Bundle;
+import android.text.format.DateFormat;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.TimePicker;
+
 import com.lahaptech.lahap.R;
 
-import android.os.Bundle;
-import android.view.View;
+import java.util.Calendar;
 
-public class IndirectOrderActivity extends AppCompatActivity implements View.OnClickListener{
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class IndirectOrderActivity extends AppCompatActivity implements View.OnClickListener {
+
+
+    @BindView(R.id.bt_showtimepicker)
+    Button btTimePicker;
+    @BindView(R.id.tv_timeresult)
+    TextView tvTimeResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_indirect_order);
+
+        ButterKnife.bind(this);
+
+        btTimePicker.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View view) {
+        if (view.getId() == R.id.bt_showtimepicker){
+            showTimeDialog();
+        }
+    }
 
+    @SuppressLint("SetTextI18n")
+    private void showTimeDialog() {
+        Calendar calendar = Calendar.getInstance();
+        TimePickerDialog timePickerDialog = new TimePickerDialog(this,
+                (view, hourOfDay, minute) ->
+                        tvTimeResult.setText("Waktu dipilih = " + hourOfDay + ":" + minute),
+                calendar.get(Calendar.HOUR_OF_DAY), calendar.get(Calendar.MINUTE),
+                DateFormat.is24HourFormat(this));
+
+
+        timePickerDialog.show();
     }
 }
