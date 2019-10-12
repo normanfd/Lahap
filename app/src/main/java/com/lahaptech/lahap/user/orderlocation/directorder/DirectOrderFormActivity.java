44c5a25dc1f78201ce9d0cbecc2b16940e0fbf16
normanfd <norman_fd@apps.ipb.ac.id>
2019-10-12 20:47:44 +0700

    save direct order to firebase

--- app/src/main/java/com/lahaptech/lahap/user/orderlocation/directorder/DirectOrderFormActivity.java
+++ app/src/main/java/com/lahaptech/lahap/user/orderlocation/directorder/DirectOrderFormActivity.java
@@ -1,19 +1,29 @@
 package com.lahaptech.lahap.user.orderlocation.directorder;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.Button;
 import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.FirebaseFirestore;
 import com.lahaptech.lahap.R;
 import com.lahaptech.lahap.model.Prevalent;
+import com.lahaptech.lahap.user.home.UserActivity;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class DirectOrderFormActivity extends AppCompatActivity {
 
-    TextView orderTableNo, username, tPrice;
+    TextView orderTableNo, username, tPrice, radiobtn;
     RadioGroup rdo_payment;
     Button btn;
 
@@ -38,14 +48,46 @@ public class DirectOrderFormActivity extends AppCompatActivity {
         btn.setOnClickListener(view -> {
             String usernameIPB = Prevalent.CurrentOnlineUser.getUsername();
             String orderType = "direct";
-            String orderStatus = "0";
-            int payMethod = rdo_payment.getCheckedRadioButtonId();
-//            savetofirebase(usernameIPB, locationID, orderTable, orderType, orderStatus, payMethod, totalAmount);
-            Log.d("radio checked", "selected " + payMethod);
+            int radioID = rdo_payment.getCheckedRadioButtonId();
+            radiobtn = findViewById(radioID);
+            String payMethod = radiobtn.getText().toString();
+            savetofirebase(usernameIPB, locationID, orderTable, orderType, payMethod, total);
+//            Log.d("radio checked", payMethod);
         });
     }
 
-    private void savetofirebase(String usernameIPB, String locationID, String orderTable) {
+    private void savetofirebase(
+            String usernameIPB, String locationID, String orderTable,
+            String orderType, String payMethod, String totalAmount) {
 
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+        Map<String, Object> order = new HashMap<>();
+        order.put("usernameIPB", usernameIPB);
+        order.put("locationID", locationID);
+        order.put("orderTime", null);
+        order.put("orderTable", orderTable);
+        order.put("orderType", orderType);
+        order.put("orderStatus", "0");
+        order.put("payMethod", payMethod);
+        order.put("transferProof", null);
+        order.put("totalAmount", totalAmount);
+
+        db.collection("order").document(usernameIPB)
+                .set(order)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Toast.makeText(DirectOrderFormActivity.this, "Success Added", Toast.LENGTH_SHORT).show();
+                        Intent intent = new Intent(DirectOrderFormActivity.this, UserActivity.class);
+                        startActivity(intent);
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+
+                    }
+                });
     }
 }
